f4b90edba4cf33bb91bbf26260cfdcbbb54c4da7
StarkLSH <lsh@zipdoc.kr>
2023-01-30 17:16:22 +0900

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer>
+      <p>© 2023 Seungheon Lee. All rights reserved.</p>
+    </footer>
+  );
+}
---
+++ src/components/Orders.jsx
@@ -0,0 +1,9 @@
+export default function Orders() {
+  return (
+    <aside>
+      <div className="empty"></div>
+      <div className="title">you don't have any orders</div>
+      <div className="subtitle">Click on a + to add an order</div>
+    </aside>
+  );
+}
